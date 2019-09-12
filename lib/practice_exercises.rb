5bbd7a9c27e00037d2bf33d92d85d626dafa6fda
Farah Davoodi <farahdavoodi@gmail.com>
2019-09-12 12:31:49 -0700

    Started pseudocode

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -3,6 +3,15 @@
 # Space Complexity: ?
 def remove_duplicates(list)
   raise NotImplementedError, "Not implemented yet"
+  # determine length of list
+  # set i = 0
+  # while i < 0 
+  #   list.each do |value|
+  #     if list[i] == value
+  #       delete value from list
+  #     end
+  #     i += 1
+  #   end
 end
 
 # Time Complexity: ?
