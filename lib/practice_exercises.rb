115abbecb2c85fb0233386e58a448dde6a119f03
Mira <calopter@gmail.com>
2019-09-13 11:06:22 -0700

    implements remove_duplicates

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,12 +2,17 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  i = 0
+  while i < list.length
+    list.delete_at(i + 1) while list[i] == list[i + 1]
+    i += 1
+  end
+  return list
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  # remove_duplicates
 end
 
