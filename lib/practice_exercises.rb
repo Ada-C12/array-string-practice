79b2b7302252884142c8ac766f8a777f6e66aae2
Dianna Tingg <dtingg@gmail.com>
2019-09-13 14:30:20 -0700

    fixed longest_prefix method

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -6,7 +6,6 @@ def remove_duplicates(list)
   
   i = 0
   
-  
   while i < list.length - 1
     if list[i] == list[i+1]
       list.delete_at(i+1)
@@ -22,6 +21,20 @@ end
 # Time Complexity: ?
 # Space Complexity: ?
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  first_string = strings[0]
+  
+  index = 0
+  continue = true
+  
+  while continue
+    strings[1..-1].each do |string|
+      if string[index] != first_string[index] || !first_string[index]
+        continue = false
+        break
+      end
+    end
+    
+    index += 1 if continue
+  end
+  return first_string[0...index]
 end
-
