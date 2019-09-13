569fa505f54fc228ea19a409fd6d79cf684d8d24
Dianna Tingg <dtingg@gmail.com>
2019-09-12 20:24:53 -0700

    implemented remove_duplicates method

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,7 +2,21 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  return list if list.length == 1 || list.empty?
+  
+  i = 0
+  
+  
+  while i < list.length - 1
+    if list[i] == list[i+1]
+      list.delete_at(i+1)
+      
+    else
+      i += 1      
+    end
+  end
+  
+  return list
 end
 
 # Time Complexity: ?
