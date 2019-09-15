5580b6f9a37e4e4341d58e253dd5f568f4f4e431
dora1405 <li.dora05@gmail.com>
2019-09-15 01:48:09 -0700

    Wave 2 complete with pseudocodes

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,18 +2,51 @@
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 def remove_duplicates(list)
-  list.length.times do |i|
-    if list[i] == list[i + 1]
-      list.delete_at i + 1
+  list.each do |num|
+    if list.include? num
+      list.delete_at num
     end
   end
-  list
 end
 
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n*m)
+# Space Complexity: O(n)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  
+  # find smallest word in array to loop through that length
+  min = strings.min_by {|word| word.length}
+  letters = []
+  min.length.times do |i|
+    strings.each do |word|
+      letters << word[i]
+    end
+  end
+  
+  # take in non-duplicated letters into another array
+  unique_letters = []
+  
+  letters.each do |letter|
+    if letters.include? letter
+      unique_letters << letter
+    end
+  end
+  
+  # find how many times a unique letter repeats and match it to the length of array
+  counts = Hash.new(0)
+  
+  letters.each do |letter|
+    counts[letter] += 1
+  end
+  
+  common_prefix = []
+  
+  counts.each do |letter, num|
+    if num == strings.length
+      common_prefix << letter
+    end
+  end
+  
+  # modify array of individual letter strings to one string
+  return common_prefix.join("")
 end
-
