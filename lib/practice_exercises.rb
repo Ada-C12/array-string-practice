db3d19a2befb36a8fdea5794b0f54935c6d05159
Vi <vanessareigosa@gmail.com>
2019-09-14 18:15:05 -0700

    implemented remove duplicates method

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,22 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n²)
+  #We only go through the list once without an inner loop, so I thought it was O(n) at first.
+  #But because I am including a 'delete_at' method, which I looked up the time complexity of and found O(n), I'm implementing another command for the algorithm to do, which is determined by O of n amount of times. 
+  #Therefore I'm assuming it is O of n squared.
+# Space Complexity: O(1)
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  i = 0
+  list.each do |i|
+    if list[i] == list[i + 1]
+      list.delete_at(i + 1)
+    end 
+  end 
+  return list 
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: 
+# Space Complexity: 
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+
 end
 
