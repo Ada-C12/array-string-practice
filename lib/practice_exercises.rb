936f9b5a813b5da2e6e8e46276915abc6025cadc
Farah Davoodi <farahdavoodi@gmail.com>
2019-09-15 16:46:56 -0700

    created method of finding longest common prefix and passed tests

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,22 +1,49 @@
+require 'pry'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) where n is the length of the array
+# Space Complexity: O(1) becasue no new array is created to hold non-duplicates. Instead the contents of the original array are overwritten while the loop is running.
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
-  # determine length of list
-  # set i = 0
-  # while i < 0 
-  #   list.each do |value|
-  #     if list[i] == value
-  #       delete value from list
-  #     end
-  #     i += 1
-  #   end
+  if list.length == 0
+    return []
+  end
+  
+  length = list.length
+  i = 0
+  j = 0
+  while i < length-1
+    if list[i] != list[i+1]
+      list[j] = list[i]
+      j += 1
+    end
+    i += 1
+  end
+  list[j] = list[length - 1]
+  list = list[0..j]
+  return list
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(m*n), where n is the length of the array and m is the length of characters of the shortest string in the array.
+# Space Complexity: O(n) because a new string is created in memory to hold the common prefix
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  common_string = ""
+  if strings.length == 0
+    return common_string
+  end
+  
+  shortest_element = strings.min
+  shortest_element_length = shortest_element.length
+  i = 0
+  while i < shortest_element_length
+    char = shortest_element[i]
+    strings.each do |string|
+      if char != string[i]
+        return common_string
+      end
+    end
+    common_string += char
+    i += 1
+  end
+  
+  return common_string
 end
 
