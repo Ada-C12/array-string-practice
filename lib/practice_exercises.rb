b4280a908afec1b24945321b1c78296cccc17841
Nicky Choi <nickyjinchoi@gmail.com>
2019-09-15 14:36:20 -0700

    created both methods and added time & space complexity

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,28 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) where n is the length of list
+# Space Complexity: O(1) slice! method changes the original array. It does not change the size of the input
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  list.each do |num|
+    if list.count(num) > 1
+      list.slice!(list.index(num))
+    end
+  end
+  return list
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n * m) n is the strings input and m is the length of the minimum string
+# Space Complexity: O(1) amount of memory used does not change as the input array changes
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  # return an empty string if the parameter is empty
+  return "" if strings.nil?
+  min = strings.min
+  max = strings.max
+  # iterates through each character in the minimum string
+  index = min.length.times do |i|
+    if min[i] != max[i]
+      break i
+    end
+  end
+  # returns the longest common prefix (excluding the index where it broke out of the loop)
+  return min[0...index]
 end
-
