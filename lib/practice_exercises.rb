5a282439a495ab32be0124f42c387cfc2c2d428e
Paige Davenport <davenport.paige@outlook.com>
2019-09-14 19:02:43 -0700

    Completed both methods.

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,50 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  length = list.length
+  i = 0
+  length.times do
+    value_1 = list[i]
+    value_2 = list[i + 1]
+    
+    if value_1 == value_2
+      list.delete_at(i)
+      i += 1
+    else 
+      i += 1
+    end
+  end
+  
+  return list
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n * m) - in this case n represents the length of the array (the each loop) 
+# and m represents the length of the first string in the array (the times loop)
+# Space Complexity: O(1)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  prefix = ""
+  i = 0
+  
+  strings[0].length.times do 
+    character = strings[0][i] 
+    matches = 0
+    
+    strings.each do |string| 
+      if character == string[i]
+        matches += 1
+      end
+    end
+    
+    if matches == strings.length
+      prefix << character
+    else
+      break
+    end
+    
+    i += 1
+  end
+  
+  return prefix
 end
 
