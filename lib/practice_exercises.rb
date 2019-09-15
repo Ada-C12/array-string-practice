3408c252c2dbb55b79f61c010dcb73a8baf0347f
Mira <calopter@gmail.com>
2019-09-14 17:35:05 -0700

    complexities

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,6 +1,6 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def remove_duplicates(list)
   i = 0
   while i < list.length
@@ -10,11 +10,11 @@ def remove_duplicates(list)
   return list
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n * m) where m = length of longest string
+# Space Complexity: O(n) where n = length of longest string
 def longest_prefix(strings)
   prefix = ->s1, s2 { s1.each_char.zip(s2.each_char)
-                        .take_while {|c1, c2| c1 == c2}
+                        .take_while { |c1, c2| c1 == c2 }
                         .map(&:first).join }
                         
   strings.reduce(&prefix)
