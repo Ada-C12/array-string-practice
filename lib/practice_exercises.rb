7bb1fe1977cda277c2f01b721349f5dd23a33e58
m.dove@live.com <m.dove@live.com>
2019-09-15 20:48:34 -0700

    Wrote method for longest prefix and for removing duplicates

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,26 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  i = 1
+  list.each do |character|
+    if character == list[i]
+      list.delete_at(i)
+    end
+    i += 1
+  end
+  return list
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2)
+# Space Complexity: 0(1)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  max_prefix = strings.min
+  max_length = max_prefix.length
+  strings.each do |string|
+    while string[0..max_length - 1] != max_prefix[0..max_length - 1]
+      max_length -= 1
+    end
+  end
+  prefix = max_prefix[0..max_length - 1]
 end
-
