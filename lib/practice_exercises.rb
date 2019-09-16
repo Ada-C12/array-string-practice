e74ac8af4410711cbeed19315686712f9295830e
brilatimer <brihiramine@gmail.com>
2019-09-16 07:33:27 -0700

    wave 1

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,24 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  if list.length == 0
+    return 0
+  end
+  unique_items = 1
+  list.length.times do |index|
+    if index != 0 && list[index - 1] != list[index]
+      unique_items +=1
+    end
+  end
+  return unique_items
 end
+
+# Did not have time to complete
 
 # Time Complexity: ?
 # Space Complexity: ?
-def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
-end
+# def longest_prefix(strings)
+#   raise NotImplementedError, "Not implemented yet"
+# end
 
