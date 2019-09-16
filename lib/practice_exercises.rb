e3a15b255fa1d91eae23ef6c05b99cad30724f35
Kristina <kristina.tanya@gmail.com>
2019-09-15 18:32:02 -0700

    Completed exercise.

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,48 @@
-
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: (On)
+# Space Complexity: O(1)
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  index = 0
+  length = list.length
+
+  while index < length do
+    while list[index] == list[index + 1] do
+      list.delete_at(index + 1)
+      length -= 1
+    end
+    index += 1
+  end
+  return list
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  tracker = strings[0]
+  matched_prefix = ""
+
+  if strings.length == 0 
+    return ""
+  end
+  index = 0
+
+ while index < tracker.length-1
+    if strings.all? { |string| string[index] == tracker[index] }
+      matched_prefix += tracker[index]
+    else
+      break
+    end 
+    index += 1
+  end
+
+  return matched_prefix
 end
+
+
+
+
+
+
+
+
+
 
