93749e0f4d68ddf726ab9bfc9fa5535c1f8ba1c1
Sara Shah Baig <sarashahbaig@gmail.com>
2019-09-16 08:50:40 -0700

    longest_prefix completed

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 # Time Complexity: o(n)
 # Space Complexity: o(1)
 
@@ -23,27 +23,29 @@ end
 # Space Complexity: ?o(1)
 
 def longest_prefix(strings)
+  # If there is no common prefix, return an empty string
   total_words = strings.length
   if total_words == 0
     return ""
   end
-
   if total_words == 1
     return strings[0]
   end
 
   longest_prefix = ""
-  total_words.times do |index|
-    word = strings[index]
-    i = index + 1
-    while i < total_words
-      next_word = strings[i]
-      if word[i] == next_word[i]
-        longest_prefix += word[i]
-        break
+  i = 0
+  strings[0].length.times do
+    word = strings[0][i]
+    common_word = 0
+    strings.each do |string|
+      if word == string[i]
+        common_word += 1
       end
-      i += 1
     end
+    if common_word == strings.length
+      longest_prefix << word
+    end
+    i += 1
   end
   return longest_prefix
 end
