ada2f099ee68341dd80df8e1934f6e159c9378ea
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-09-15 17:13:23 -0700

    Both methods passing all tests, including the one suggested by Sabrina that I added to the tests file.

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,57 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  i = 0
+  list.length.times do
+    if list[i] == list[i + 1]
+      list.delete_at(i)
+      i += 1
+    else
+      i += 1
+    end
+  end
+  return list
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: although there is a loop within a loop, I don't think it's quadratic 
+# because adding a word to the strings array doesn't mean doubling the total operations 
+# ... so I'm going to go with good old O(n)
+# Space Complexity: O(1)
+
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  prefix = ""
+  i = 0
+  
+  # go through the array of words, and for each letter in the first word ...
+  while i < strings[0].length
+    # assign the letter at index[i] to variable "letter"
+    letter = strings[0][i]
+    # create counter for matching letters
+    matches = 0
+    # now, for each of the words in the strings array ...
+    strings.each do |string|
+      # if the letter at the first word's index[i] matches the letter at index[i] in the word being compared ...
+      if letter == string[i]
+        # ... increment the matches counter
+        matches += 1
+      end
+    end
+    
+    # if the number of matches equals the number of words in the array
+    if matches == strings.length
+      # ... add the letter to the "longest prefix" variable
+      prefix += letter
+    else
+      # otherwise, stop the loop
+      break
+    end
+    
+    # increment the index
+    i += 1
+  end
+  
+  return prefix
+  
 end
 
--- test/practice_exercises_test.rb
+++ test/practice_exercises_test.rb
@@ -9,35 +9,42 @@ describe "Practice Exercises" do
     it "works for empty arrays" do
       expect(remove_duplicates([])).must_equal []
     end
-
+    
     it "will remove duplicates for longer arrays" do
       expect(remove_duplicates([1, 2, 2, 3, 3, 4])).must_equal [1, 2, 3, 4]
     end
   end
-
+  
   describe "Longest valid substring" do
     it "will work for the README strings" do
       strings = ["flower","flow","flight"]
-
+      
       output = longest_prefix(strings)
-
+      
       expect(output).must_equal "fl"
     end
-
+    
     it "will work for the strings with the common prefix in the rear" do
       strings = ["flower","flow","flight", "fpastafl"]
-
+      
       output = longest_prefix(strings)
-
+      
       expect(output).must_equal "f"
     end
-
+    
     it "will work for the README strings" do
       strings = ["dog","racecar","car"]
-
+      
       output = longest_prefix(strings)
-
+      
       expect(output).must_equal ""
     end
+    
+    it "will work on whole-word matches" do
+      strings = ["flower", "flower", "flower"]
+      output = longest_prefix(strings)
+      expect(output).must_equal "flower"
+    end
+    
   end
 end
