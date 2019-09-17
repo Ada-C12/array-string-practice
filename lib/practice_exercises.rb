60e594da5aa726058f4478881c15e47e939e72ea
emaust <erikamaust@gmail.com>
2019-09-16 20:10:56 -0700

    Implemented longest_prefix

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 # Time Complexity: O(n)
 # Space Complexity: O(n)
 def remove_duplicates(list)
@@ -14,16 +14,32 @@ end
 
 
 
+# Time Complexity: O(n * m)
+# Space Complexity: O(1)
 
-# Space Complexity: ?
-# def longest_prefix(strings)
-#   first_word = strings[0]
-#   length = strings.length
-#   length -= length
+def longest_prefix(strings)
+  prefix = strings[0]
+  original = prefix
+  a_length = strings.length
+  a_length -= 1
+  
+  
+  a_length.times do |index|
+    comparison_word = strings[index + 1]
+    w_length = prefix.length
+    w_length.times do |i|
+      if prefix[i] != comparison_word[i] 
+        i -= 1
+        prefix = prefix[0..i]
+        if prefix == original
+          return ""
+        end
+        break
+      end
+    end
+  end
+  return prefix
+end
 
-#   length.times do |index|
-#     comparison_word = array[index + 1]
-
-# end
 
 
