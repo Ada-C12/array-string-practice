1ff59c6db2b4dfdc70f6e1abc091c214f6db5605
Mariya Burrows <mariya.burrows@gmail.com>
2019-09-16 18:15:01 -0700

    completed cs homework

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,34 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  list.each do |i|
+    if list[i] == list[i + 1]
+      list.delete_at(i)
+    end
+  end
+  return list
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  loops = (strings.length) - 1
+  comparison_word = strings[0]
+  
+  loops.times do |i|
+    prefix = ""
+    counter = 0
+    
+    strings[i + 1].each_char do |letter|
+      if letter == comparison_word[counter]
+        prefix<<(letter)
+      end
+      counter += 1
+    end
+    comparison_word = prefix
+  end
+  
+  return comparison_word
 end
 
