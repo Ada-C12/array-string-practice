643961fbdf22493a03e7d53771983816d62ce521
emaust <erikamaust@gmail.com>
2019-09-16 18:45:30 -0700

    Remove duplicates completed

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,29 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def remove_duplicates(list)
-  raise NotImplementedError, "Not implemented yet"
+  index = 0
+  while index <= list.length
+    if list[index] == list[index + 1]
+      list.delete_at(index)
+    end
+    index += 1
+  end
+  return list
 end
 
-# Time Complexity: ?
+
+
+
 # Space Complexity: ?
-def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
-end
+# def longest_prefix(strings)
+#   first_word = strings[0]
+#   length = strings.length
+#   length -= length
+
+#   length.times do |index|
+#     comparison_word = array[index + 1]
+
+# end
+
 
