da58fae40ca4f7009d3237d3a99fd2aa920f5add
gracexinran <gracexinran@gmail.com>
2019-09-17 22:22:00 -0700

    refactored code

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -7,23 +7,16 @@ def remove_duplicates(list)
   return list if list.empty?
   i = 0
   j = 0
-  largest_num = list[i]
-  final_largest_num = list.last
-  until largest_num == final_largest_num
-    if list[i] <= list[j] && list[j] > largest_num
-      i += 1 if i == 0
-      temp = list[i]
-      list[i] = list[j]
-      list[j] = temp
-      largest_num = list[i]
+  until j == list.length
+    if list[i] < list[j]
+      temp = list[i + 1]
+      list[i + 1] = list[j]
+      list[j] = list[i + 1]
       i += 1
-      j += 1
-    else
-      j += 1 if j < list.length - 1
     end
+    j += 1
   end
-  return list[0..(i-1)] if i > 1
-  return [list[0]] if i == 0
+  return list[0..i]
 end
 
 
